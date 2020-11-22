0f74b689e185261fb9f1e4f38588ec423e69ab85
lukewittenkeller <lwitte3@uic.edu>
2020-11-22 01:01:29 -0600

    Scheduler class is almost done. not sure how we can make it more flexible to have a time for each day

--- app/src/main/java/com/example/mymanifit/Scheduler.java
+++ app/src/main/java/com/example/mymanifit/Scheduler.java
@@ -3,11 +3,19 @@ package com.example.mymanifit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
-public class Scheduler extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class Scheduler extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+    Spinner s1, s2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,6 +23,29 @@ public class Scheduler extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scheduler);
 
+        s1 = (Spinner) findViewById(R.id.spinner1);
+        s2 = (Spinner) findViewById(R.id.spinner2);
+
+        ArrayAdapter<CharSequence> hourAdapter = ArrayAdapter.createFromResource(this, R.array.hours, android.R.layout.simple_spinner_item);
+        hourAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s1.setAdapter(hourAdapter);
+        s1.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> minsAdapter = ArrayAdapter.createFromResource(this, R.array.minutes, android.R.layout.simple_spinner_item);
+        minsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s2.setAdapter(minsAdapter);
+        s2.setOnItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
