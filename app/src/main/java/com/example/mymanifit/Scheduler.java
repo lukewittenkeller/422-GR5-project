b6eb4f35d91ae8ef8ed9c637ed3b17533e398eae
lukewittenkeller <lwitte3@uic.edu>
2020-11-22 02:49:42 -0600

    Scheduler class is almost done. not sure how we can make it more flexible to have a time for each day

--- app/src/main/java/com/example/mymanifit/Scheduler.java
+++ app/src/main/java/com/example/mymanifit/Scheduler.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -14,8 +15,9 @@ import java.util.ArrayList;
 
 public class Scheduler extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-
+    CheckBox mon, tues, wed, thurs, fri, sat, sun, am, pm;
     Spinner s1, s2;
+    Button next, back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,6 +27,15 @@ public class Scheduler extends AppCompatActivity implements AdapterView.OnItemSe
 
         s1 = (Spinner) findViewById(R.id.spinner1);
         s2 = (Spinner) findViewById(R.id.spinner2);
+        next = (Button) findViewById(R.id.nextButton);
+        back = (Button) findViewById(R.id.backButton);
+        sun = (CheckBox) findViewById(R.id.checkBox1);
+        mon = (CheckBox) findViewById(R.id.checkBox2);
+        tues = (CheckBox) findViewById(R.id.checkBox3);
+        wed = (CheckBox) findViewById(R.id.checkBox4);
+        thurs = (CheckBox) findViewById(R.id.checkBox5);
+        fri = (CheckBox) findViewById(R.id.checkBox6);
+        sat = (CheckBox) findViewById(R.id.checkBox7);
 
         ArrayAdapter<CharSequence> hourAdapter = ArrayAdapter.createFromResource(this, R.array.hours, android.R.layout.simple_spinner_item);
         hourAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
