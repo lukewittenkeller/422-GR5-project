6c49b7ade7e23dc809015e0b8408d666af271dc7
lukewittenkeller <lwitte3@uic.edu>
2020-11-23 13:59:33 -0600

    added btn fucntionality in Workout.java

--- app/src/main/java/com/example/mymanifit/Workout.java
+++ app/src/main/java/com/example/mymanifit/Workout.java
@@ -4,7 +4,9 @@ import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +18,7 @@ import java.util.Locale;
 public class Workout extends AppCompatActivity {
     TextView timerText;
     TextView workoutText;
+    Button editBtn, viewBtn;
     CountDownTimer timer;
     boolean timerRunning;
     long timeLeft;
@@ -30,6 +33,31 @@ public class Workout extends AppCompatActivity {
         timeLeft = 30000;
         workoutText = (TextView) findViewById(R.id.textViewWorkout);
         timerText = (TextView) findViewById(R.id.countdown);
+        editBtn = (Button) findViewById(R.id.editBtn);
+        viewBtn = (Button) findViewById(R.id.viewBtn);
+
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(Workout.this, WorkoutInfo.class);
+                startActivity(i);
+            }
+        });
+
+        viewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(Workout.this, Scheduler.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
+
         ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
         ObjectAnimator animation = ObjectAnimator.ofInt(progressBar, "progress", 100, 0); // see this max value coming back here, we animate towards that value
         animation.setDuration(30000); // in milliseconds
